f6c9d595ca8ab655d3faeb86e5c17c107409ecd2
harshanand2110 <harshanand2110@gmail.com>
2026-04-24 13:56:20 +0530

    Added encapsulation example files

--- src/OOPS/Encapsulation/Demo.java
+++ src/OOPS/Encapsulation/Demo.java
@@ -1,5 +1,19 @@
 package OOPS.Encapsulation;
 
+import OOPS.Student;
+
 public class Demo {
+    public static void main(String[] args) {
+
+        Student A = new Student(21,19,"Harsh",022, "Shanaya");
+        System.out.println(A.id);
+        System.out.println(A.roll);
+        System.out.println(A.name);
+        System.out.println(A.age);
+        System.out.println(A.getGf());  //getter is used check Student class to get more idea
+        A.eat();
+        A.sleep();
+        A.gfChatting();
+    }
     
 }
